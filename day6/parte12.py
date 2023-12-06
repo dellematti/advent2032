ef04c6268493a4f55b3a899f06e6f1e8462705b6
dellematti <dellematti@hotmail.it>
2023-12-06 13:06:54 +0100

    Şase

---
+++ day6/parte12.py
@@ -0,0 +1,35 @@
+def metodiVittoria(tempo, spazio) :
+    sum =0
+    for velocità in range (tempo):
+        distanza = velocità*(tempo-velocità)
+        if distanza > spazio:
+            sum += 1
+            if sum>0 and distanza <= spazio:
+                return sum
+    return sum
+
+
+def numeriInRiga ( riga):
+        return [int(s) for s in riga.split() if s.isdigit()]
+
+
+
+file = "/home/delle/Scrivania/Programmi/advent2032/day6/input.txt"
+time = []
+space = []
+with open(file, 'r+') as f:
+    for idx, line in enumerate (f):
+        if idx == 0:
+            time = numeriInRiga(line)
+        if idx == 1:
+             space = numeriInRiga(line)
+
+# print(time)
+
+
+output = 1
+for i in range (len(time)):
+    output *= metodiVittoria(time[i], space[i])
+    # print(metodiVittoria(time[i], space[i]))
+
+print(output)
