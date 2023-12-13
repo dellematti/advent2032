d25bb3bca3ae37cb29e55217b7889c8c35aef14c
dellematti <dellematti@hotmail.it>
2023-12-13 22:27:43 +0100

    trinajst

--- day13/parte2.py
+++ day13/parte2.py
@@ -1,5 +1,5 @@
 def posizioneRiflesso(matrice):
-    # faccio i controlli per ogni possibile colonna di partenza (ipotetico centro) 
+    # faccio i controlli per ogni possibile colonna e riga di partenza (ipotetici centri) 
     for i in range(len(matrice[0])-1):
         if controlloColonna(i,i+1, matrice,False):
             return i+1
@@ -9,49 +9,38 @@ def posizioneRiflesso(matrice):
     return 0
 
 
-def controlloRiga (sopra, sotto, matrix, flag):
-    numeroDifferenze = 0
+def controlloRiga (sopra, sotto, matrix, unaSolaRigaDiversa):
+    caratteriDifferenti = 0
     for i in range(len(matrix[sopra])):
         if matrix[sopra][i] != matrix[sotto][i]:
-            numeroDifferenze += 1
-    if numeroDifferenze > 1 or (flag and numeroDifferenze == 1):
+            caratteriDifferenti += 1
+    if caratteriDifferenti > 1 or (unaSolaRigaDiversa and caratteriDifferenti == 1):
         return False
-    if numeroDifferenze == 1:
-        flag = True
-
-    if matrix[sopra] == matrix[sotto] or numeroDifferenze == 1:    
+    if caratteriDifferenti == 1:
+        unaSolaRigaDiversa = True
+    if matrix[sopra] == matrix[sotto] or caratteriDifferenti == 1:    
         if sopra == 0 or sotto == len(matrix)-1:
-            if flag:
-                return True
-            else:
-                return False
+            return unaSolaRigaDiversa
         else:
-            return controlloRiga(sopra-1,sotto+1, matrix, flag)
-    return False
+            return controlloRiga(sopra-1,sotto+1, matrix, unaSolaRigaDiversa)
 
 
-def controlloColonna (sinistra, destra, matrix,flag):
-    numeroDifferenze = 0
+def controlloColonna (sinistra, destra, matrix,unaSolaColonnaDiversa):
+    caratteriDifferenti = 0
     colonnaSinistra = column(matrix, sinistra)
     colonnaDestra = column(matrix, destra)
     for i in range(len(colonnaSinistra)):
         if colonnaSinistra[i] != colonnaDestra[i]:
-            numeroDifferenze += 1
-
-    if numeroDifferenze > 1 or (flag and numeroDifferenze == 1):
+            caratteriDifferenti += 1
+    if caratteriDifferenti > 1 or (unaSolaColonnaDiversa and caratteriDifferenti == 1):
         return False
-    if numeroDifferenze == 1:
-        flag = True
-
-    if colonnaSinistra == colonnaDestra or numeroDifferenze == 1:    
+    if caratteriDifferenti == 1:
+        unaSolaColonnaDiversa = True
+    if colonnaSinistra == colonnaDestra or caratteriDifferenti == 1:    
         if sinistra == 0 or destra == len(matrix[0])-1 :
-            if flag:
-                return True
-            else:
-                return False
+            return unaSolaColonnaDiversa
         else:
-            return controlloColonna(sinistra-1,destra+1, matrix, flag)
-    return False
+            return controlloColonna(sinistra-1,destra+1, matrix, unaSolaColonnaDiversa)
 
 
 def column(matrix, i):
@@ -69,11 +58,11 @@ with open(fileName, 'r+') as f:
     riga = 0
     for line in f:
         if line == "\n" :    # se la riga è vuota
-            output += (posizioneRiflesso(matrice))
+            output += posizioneRiflesso(matrice)
             matrice.clear()
         else:
             matrice.append(line.strip())
         riga += 1
-output += (posizioneRiflesso(matrice))
+output += posizioneRiflesso(matrice)
 print(output)
 # 38063
